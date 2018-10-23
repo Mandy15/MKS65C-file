ccd401e64d2e53846c45b5812c8ded437fd1cf25
Mandy <mmai1@stuy.edu>
2018-10-23 15:36:21 -0400

    update

--- file.c
+++ file.c
@@ -1,3 +1,18 @@
-int main(){
+#include<stdio.h>
+#include <stdlib.h>
+#include<fcntl.h>
+// #include<sys/types.h>
+// #include<sys/stat.h>
+// #include<errno.h>
 
+int main(){
+  int fd = open("file.txt", O_RDONLY | O_CREAT);
+  printf("Opened file.txt: %d\n", fd);
+
+  char *buf = malloc(10);
+  printf("Read %d bytes in file.txt:\n%s\n", read(fd, buf, 10), buf);
+
+  // write();
+
+  printf("Closed file.txt: %d\n", close(fd));
 }
